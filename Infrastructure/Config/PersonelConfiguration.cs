0f3398cf2d8f090ee36a92b9a5b441f454c64d24
ethem yilmaz <ethem_yilmaz95@icloud.com>
2024-03-26 14:12:58 +0300

    Entities, Data ve Config oluşturuldu

---
+++ Infrastructure/Config/PersonelConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using HRTechProject.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Config
+{
+    public class PersonelConfiguration : IEntityTypeConfiguration<Personel>
+    {
+        public void Configure(EntityTypeBuilder<Personel> builder)
+        {
+            builder.Property(x => x.Ad).HasMaxLength(100);
+            builder.Property(x => x.Soyad).HasMaxLength(100);
+            builder.Property(x => x.TcNo).HasMaxLength(11);
+            builder.Property(x => x.Maas).HasPrecision(18, 2);
+        }
+    }
+}
