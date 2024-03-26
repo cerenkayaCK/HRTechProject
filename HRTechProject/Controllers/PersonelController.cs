660c5c55518f385ed07d04231fd103978e439adc
hakanpakelgil <hakanpakelgil@hotmail.com>
2024-03-26 18:34:32 +0300

    Personel Anasayfası Düzenlendi

---
+++ HRTechProject/Controllers/PersonelController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HRTechProject.Controllers
+{
+    public class PersonelController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {            
+            return View();
+        }
+    }
+}
