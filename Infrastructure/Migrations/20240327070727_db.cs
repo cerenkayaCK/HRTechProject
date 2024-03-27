06f9cf1aacfbd1435dca8318e3bbaee5281eb228
Alican Dökmen <alican@outlook.com>
2024-03-27 10:08:13 +0300

    db ekleme sonrası migrate edilmesi

---
+++ Infrastructure/Migrations/20240327070727_db.cs
@@ -0,0 +1,194 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class db : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Telefon",
+                table: "Personeller",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Soyad2",
+                table: "Personeller",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Soyad",
+                table: "Personeller",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Personeller",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Ad2",
+                table: "Personeller",
+                type: "nvarchar(40)",
+                maxLength: 40,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Ad",
+                table: "Personeller",
+                type: "nvarchar(40)",
+                maxLength: 40,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<int>(
+                name: "DepartmanId",
+                table: "Personeller",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SirketId",
+                table: "Personeller",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Personeller_DepartmanId",
+                table: "Personeller",
+                column: "DepartmanId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Personeller_SirketId",
+                table: "Personeller",
+                column: "SirketId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Personeller_Departmanlar_DepartmanId",
+                table: "Personeller",
+                column: "DepartmanId",
+                principalTable: "Departmanlar",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Personeller_Sirketler_SirketId",
+                table: "Personeller",
+                column: "SirketId",
+                principalTable: "Sirketler",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Personeller_Departmanlar_DepartmanId",
+                table: "Personeller");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Personeller_Sirketler_SirketId",
+                table: "Personeller");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Personeller_DepartmanId",
+                table: "Personeller");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Personeller_SirketId",
+                table: "Personeller");
+
+            migrationBuilder.DropColumn(
+                name: "DepartmanId",
+                table: "Personeller");
+
+            migrationBuilder.DropColumn(
+                name: "SirketId",
+                table: "Personeller");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Telefon",
+                table: "Personeller",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(60)",
+                oldMaxLength: 60);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Soyad2",
+                table: "Personeller",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(60)",
+                oldMaxLength: 60,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Soyad",
+                table: "Personeller",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(60)",
+                oldMaxLength: 60);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Personeller",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Ad2",
+                table: "Personeller",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(40)",
+                oldMaxLength: 40,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Ad",
+                table: "Personeller",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(40)",
+                oldMaxLength: 40);
+        }
+    }
+}
